88290b635daf1bf0cbdb7ff8487ea2f4e4e762c8
Michael Henke <michaelh1995@gmx.de>
2023-07-12 11:06:24 +0200

    handle submit event, reset/focus input field, activities add state, add function handleActivities, install/add uid from react, form onAddActivity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import { Form } from "./components/Form";
+import { uid } from "uid";
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(activity) {
+    setActivities([...activities, { ...activity, id: uid() }]);
+  }
+  console.log("activities:", activities);
   return (
     <div className="App">
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,28 @@
-export function Form() {
+export function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.name.value, event.target.isForGoodWeather.checked);
+    onAddActivity({
+      name: event.target.name.value,
+      isForGoodWeather: event.target.isForGoodWeather.checked,
+    });
+    event.target.reset();
+    event.target.name.focus();
+  }
   return (
     <>
       <h1>Heading</h1>
-      <label htmlFor="name">Activity name</label>
-      <input id="name" type="text" name="name"></input>
-      <label htmlFor="is-for-good-weather">Is for good weather?</label>
-      <input
-        id="is-for-good-weather"
-        type="checkbox"
-        name="isForGoodWeather"
-      ></input>
-      <button type="submit">Submit!</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Activity name</label>
+        <input id="name" type="text" name="name"></input>
+        <label htmlFor="is-for-good-weather">Is for good weather?</label>
+        <input
+          id="is-for-good-weather"
+          type="checkbox"
+          name="isForGoodWeather"
+        ></input>
+        <button type="submit">Submit!</button>
+      </form>
     </>
   );
 }
