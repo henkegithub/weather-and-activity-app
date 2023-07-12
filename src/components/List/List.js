30e39299d6aa3c41486607206cf777b7e2b72d3e
Dmitry Nosov <nosov.sibers@gmail.com>
2023-07-12 14:19:04 +0200

    Add the ability to remove activities

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,16 @@
-export function List({ activities }) {
+export function List({ activities, onDeleteActivity }) {
   return (
     <>
       <ul>
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.name}</li>
+          <li key={activity.id}>
+            {activity.name}
+            <button type="button" onClick={() => onDeleteActivity(activity.id)}>
+              <span role="img" aria-label="Remove">
+                ❌
+              </span>
+            </button>
+          </li>
         ))}
       </ul>
     </>
