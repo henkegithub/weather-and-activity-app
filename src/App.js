a11aac7859da669aa8ce299f1f0cf94f477bec36
Dmitry Nosov <nosov.sibers@gmail.com>
2023-07-12 13:01:51 +0200

    Add weather widget by fetching the weather API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-import { Form } from "./components/Form";
+import { Form } from "./components/Form/Form";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
-import { List } from "./components/List";
+import { List } from "./components/List/List";
+import { ListHeading } from "./components/ListHeading/ListHeading";
+import { useEffect, useState } from "react";
+import { WeatherWidget } from "./components/WeatherWidget/WeatherWidget";
 function App() {
+  const [weather, setWeather] = useState();
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [
       {
@@ -11,8 +15,24 @@ function App() {
         isForGoodWeather: true,
         id: "dd4350364bd",
       },
+      {
+        name: "Wash dishes",
+        isForGoodWeather: false,
+        id: "dd4350364ba",
+      },
     ],
   });
+
+  useEffect(() => {
+    async function getWeather() {
+      const weatherData = await (
+        await fetch("https://example-apis.vercel.app/api/weather")
+      ).json();
+      console.log("weatherData", weatherData);
+      setWeather(weatherData);
+    }
+    getWeather();
+  }, []);
 
   function handleAddActivity(activity) {
     setActivities([...activities, { ...activity, id: uid() }]);
@@ -20,8 +40,17 @@ function App() {
   console.log("activities:", activities);
   return (
     <div className="App">
+      <WeatherWidget
+        weatherEmoji={weather?.condition}
+        temperature={weather?.temperature}
+      />
+      <ListHeading isGoodWeather={!!weather?.isGoodWeather} />
+      <List
+        activities={activities.filter(
+          (activity) => activity.isForGoodWeather === !!weather?.isGoodWeather
+        )}
+      />
       <Form onAddActivity={handleAddActivity} />
-      <List activities={activities} />
     </div>
   );
 }
